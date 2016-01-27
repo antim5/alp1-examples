ecdf814f0bc18ed05a6a448956982c9ac97037e5
andrey.timofeyev <aatimofeyev@gmail.com>
2016-01-27 12:57:27 +0400

    dev cp

---
+++ src/alp1/examples/Alp1Examples.java
@@ -0,0 +1,18 @@
+package alp1.examples;
+
+/**
+ * A set of examples for common algorithms implementation
+ * @author andrey
+ */
+public class Alp1Examples {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
